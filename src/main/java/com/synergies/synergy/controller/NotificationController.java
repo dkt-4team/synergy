6f5a2cd5fb01a2f1a0e7968e8975a7a61568fd49
num1dev <num1developer@naver.ocm>
2023-05-03 17:30:11 +0900

    [SY-41] Update : 공지사항 교수자 등록 기능 변경

--- src/main/java/com/synergies/synergy/controller/NotificationController.java
+++ src/main/java/com/synergies/synergy/controller/NotificationController.java
@@ -1,6 +1,9 @@
 package com.synergies.synergy.controller;
 
+import com.synergies.synergy.domain.dto.AssignmentDto;
 import com.synergies.synergy.domain.dto.NotificationDto;
+import com.synergies.synergy.domain.vo.AssignmentVo;
+import com.synergies.synergy.service.AssignmentService;
 import com.synergies.synergy.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,18 +20,34 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
+    @Autowired
+    private AssignmentService assignmentService;
+
     @GetMapping("/adminMain")
     public String getAll(Model model) {
         List<NotificationDto> notiList = notificationService.getAll();
+        List<AssignmentVo> assigmentList = assignmentService.getTodayAssignment();
+        model.addAttribute("AssignmentDTO", new AssignmentDto());
+
+        // 공지 데이터 불러오기
         if (notiList.isEmpty()){
             model.addAttribute("notificationList", null);
+            model.addAttribute("assignmentList", assigmentList);
             return "adminMain";
         }
         for (NotificationDto notification : notiList) {
             notification.setRegDate(notification.formattedRegDate());
         }
-
         model.addAttribute("notiList", notiList);
+        
+        // 오늘 등록된 과제 데이터 불러오기
+        if (assigmentList.isEmpty()) {
+            model.addAttribute("assignmentList", null);
+        }
+        else {
+            model.addAttribute("assignmentList", assigmentList);
+        }
+        
         return "adminMain";
     }
 
