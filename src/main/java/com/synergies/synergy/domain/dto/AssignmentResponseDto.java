c19ee9f7b41e9994b9c464bf9cf8a97717ade42e
yewonkang00 <yewon00000@naver.com>
2023-05-05 23:22:16 +0900

    [SY-27] Feat : 과제 상세 내용 데이터 전송을 위한 responseDto 추가

---
+++ src/main/java/com/synergies/synergy/domain/dto/AssignmentResponseDto.java
@@ -0,0 +1,27 @@
+package com.synergies.synergy.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+public class AssignmentResponseDto {
+
+    @Getter
+    @Setter
+    public static class AssignmentDetail {
+        private int id;
+        private String title;
+    }
+
+    @Getter
+    @Setter
+    public static class AssignmentContent {
+        private int id;
+        private String title;
+        private String content;
+        private String file;
+    }
+
+
+}
