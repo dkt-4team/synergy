a3a4d952167285914ce60aaf3e83f1826c78ca19
yonguk929@gmail.com <yonguk929@gmail.com>
2023-05-14 18:29:34 +0900

    Update : 회원가입, 로그인 후 유저 정보 확인을 위한 로그 추가

--- src/main/java/com/synergies/synergy/domain/dto/SignupDto.java
+++ src/main/java/com/synergies/synergy/domain/dto/SignupDto.java
@@ -2,9 +2,11 @@ package com.synergies.synergy.domain.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class SignupDto {
     private byte[] id;
     private String userId;
